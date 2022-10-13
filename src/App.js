e0f42a2c5e89f63e14dca8deb867ce46058a0877
Hemadurga1464 <74001821+Hemadurga1464@users.noreply.github.com>
2022-10-13 13:15:03 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+
+Amplify.configure(awsconfig)
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <AmplifySignOut />
+        <h2> My App Content</h2>
+      </header>
+      
+    </div>
+  );
+}
+
+export default withAuthenticator(App);
